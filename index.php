447f786e0a02642919da9e4a7a7d09b3bf8711a6
MOLE-DV <maxx.olejnik@gmail.com>
2023-12-12 14:28:07 +0100

    Licznik i wyświetlanie

--- index.php
+++ index.php
@@ -3,27 +3,79 @@
     decodePesel(fread(fopen("text.txt", "r"), 8192));
 
     function decodePesel($pesel){
-        if(!is_numeric($pesel)) errorHandler(1);
-        if(strlen($pesel) < 11) errorHandler(2);
+        if(!is_numeric($pesel)) errorHandler(1); // ciąg znaków nie składa się z liczb
+        if(strlen($pesel) != 11) errorHandler(2); // czy długość ciągu jest wystarczająca (>11)
 
         $peselSplitted = str_split(strval($pesel));
 
         $preyr = $peselSplitted[2] >= 2 ? 20 : 19;
 
         $dane = array(
-            intval($preyr.$peselSplitted[0].$peselSplitted[1]),
-            $preyr > 19 ? (intval($peselSplitted[2] . $peselSplitted[3]) - 20) : intval($peselSplitted[2] . $peselSplitted[3]),
-            intval($peselSplitted[4] . $peselSplitted[5]),
-            $peselSplitted[count($peselSplitted) - 2] % 2 == 0 ? "Kobieta" : "Mężczyzna"
+            intval($preyr.$peselSplitted[0].$peselSplitted[1]), // Rok
+            $preyr > 19 ? (intval($peselSplitted[2] . $peselSplitted[3]) - 20) : intval($peselSplitted[2] . $peselSplitted[3]), //miesiąc
+            intval($peselSplitted[4] . $peselSplitted[5]), //dzień
+            $peselSplitted[count($peselSplitted) - 2] % 2 == 0 ? "Kobieta" : "Mężczyzna" //płeć
         );
 
-        print_r($dane);
+        echo "$dane[3] urodzo".($dane[3] == "Mężczyzna" ? "ny" : "na")." w "
+        .(getNameOfDay(strtotime("$dane[0]-$dane[1]-$dane[2]")))." "
+        ."$dane[2]go ".getNameOfMonth(strtotime("$dane[0]-$dane[1]-$dane[2]"))." "
+        ."$dane[0] roku."." "
+        ."Do urodzin pozostało: "
+        . getDiffToBirthday("$dane[0]-$dane[1]-$dane[2]") ." dni"
+        .", czyli ". getDiffToBirthday("$dane[0]-$dane[1]-$dane[2]") * 24 ." godzin"
+        .", czyli ". getDiffToBirthday("$dane[0]-$dane[1]-$dane[2]") * 24 * 60 ." minut"
+        .", czyli ". getDiffToBirthday("$dane[0]-$dane[1]-$dane[2]") * 24 * 60 * 60 ." sekund";
     }
 
-    function decodeYear($pesel){
+    function getDiffToBirthday($birthDate){
+        $todaysDate = new DateTime();
 
-        return 0;
+        $nextBirthDay = new DateTime($birthDate);
+        $nextBirthDay->setDate($todaysDate->format("Y"), $nextBirthDay->format("m"), $nextBirthDay->format("d"));
+
+        if($todaysDate->format('Y-m-d') > $nextBirthDay->format('Y-m-d')){
+            $nextBirthDay->modify("+1 year");
+        }
+
+        $interval = $todaysDate->diff($nextBirthDay);
+
+        return ($interval->days) + 1;
     }
+
+    function getNameOfDay($date){
+        $tranNames = array(
+            "monday" => "poniedziałek",
+            "tuesday" => "wtorek",
+            "wednesday" => "środe",
+            "thursday" => "czwartek",
+            "friday" => "piątek",
+            "saturday" => "sobote",
+            "sunday" => "niedziele"
+        );
+
+        return $tranNames[strtolower(date("l", $date))];
+    }
+
+    function getNameOfMonth($date){
+        $monthDictionary = array(
+            "january" => "stycznia",
+            "february" => "lutego",
+            "march" => "marca",
+            "april" => "kwietnia",
+            "may" => "maja",
+            "june" => "czerwca",
+            "july" => "lipca",
+            "august" => "sierpnia",
+            "september" => "września",
+            "october" => "października",
+            "november" => "listopada",
+            "december" => "grudnia"
+        );
+
+        return $monthDictionary[strtolower(date("F", $date))];
+    }
+
 
     function errorHandler($errorCode){
         switch($errorCode){
